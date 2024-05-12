9cd04eb5e8d0b8a3c4e421dccefe7792d01e9d44
EthanNC <ethan.n.cumming@gmail.com>
2024-05-12 16:09:58 -0400

    sst init

---
+++ sst.config.ts
@@ -0,0 +1,16 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "shelf",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Remix("MyWeb", {
+      buildCommand: "npm run build",
+    });
+  },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -47,34 +47,34 @@ export default defineConfig({
       exclude: [/^\/(app)\/.+/, /^\/@.+$/, /^\/node_modules\/.*/],
     }),
     remix({
-      serverBuildFile: "remix.js",
+      // serverBuildFile: "remix.js",
       ignoredRouteFiles: ["**/.*"],
       routes: async (defineRoutes) => {
         return flatRoutes("routes", defineRoutes);
       },
-      buildEnd: async () => {
-        await esbuild
-          .build({
-            alias: { "~": "./app" },
-            // The final file name
-            outfile: "build/server/index.js",
-            // Our server entry point
-            entryPoints: ["server/index.ts"],
-            // Dependencies that should not be bundled
-            // We import the remix build from "../build/server/remix.js", so no need to bundle it again
-            external: ["./build/server/*"],
-            platform: "node",
-            format: "esm",
-            // Don't include node_modules in the bundle
-            packages: "external",
-            bundle: true,
-            logLevel: "info",
-          })
-          .catch((error: unknown) => {
-            console.error(error);
-            process.exit(1);
-          });
-      },
+      // buildEnd: async () => {
+      //   await esbuild
+      //     .build({
+      //       alias: { "~": "./app" },
+      //       // The final file name
+      //       outfile: "build/server/index.js",
+      //       // Our server entry point
+      //       entryPoints: ["server/index.ts"],
+      //       // Dependencies that should not be bundled
+      //       // We import the remix build from "../build/server/remix.js", so no need to bundle it again
+      //       external: ["./build/server/*"],
+      //       platform: "node",
+      //       format: "esm",
+      //       // Don't include node_modules in the bundle
+      //       packages: "external",
+      //       bundle: true,
+      //       logLevel: "info",
+      //     })
+      //     .catch((error: unknown) => {
+      //       console.error(error);
+      //       process.exit(1);
+      //     });
+      // },
     }),
     tsconfigPaths(),
   ],
